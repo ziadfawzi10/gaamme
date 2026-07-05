750260668dd9bbdb08096c1a7946e8670429e2b7
Źìàď Fàwźy <fawziziad58@gmail.com>
2026-07-05 18:23:01 +0300

    gaamme

---
+++ story_content/5q80tl6FSZw_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5q80tl6FSZw_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.120.37241.0%0D%0A%0D%0A00%3A00%3A00.100%20--%3E%2000%3A00%3A00.970%0D%0A%5Bopen%20game%20button%5D%0D%0A%0D%0A"}]})
